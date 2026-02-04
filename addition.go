1419799fb3d6cb047b1ad25217ce0350e6f595d5
adrian <adrian.trillo@wallbox.com>
2026-02-04 07:51:50 +0100

    add godocs

--- addition.go
+++ addition.go
@@ -1,5 +1,12 @@
+// Package addition provides basic arithmetic operations.
+
 package addition
 
+// Add returns the sum of two integers.
+// If the result overflows the int type, it will wrap around according to Go's spec.
+// More information can be found at [Addition].
+//
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
